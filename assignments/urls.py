6dde59b690d53e1f14bf177ac7f39528394e3b92
Shrirang-Amle <shrirang.amle2004@gmail.com>
2026-02-14 17:51:31 +0530

    Cleaned requirements

--- assignments/urls.py
+++ assignments/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.set_preferences, name='home'),
     path('set/', views.set_preferences, name='set_preferences'),
     path('show/', views.show_preferences, name='show_preferences'),
     path('submit/', views.submit_assignment, name='submit_assignment'),
